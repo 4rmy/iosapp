56e714e5c5f202786eb027df0f6152ab2e80a9b5
4rmy <hjacob215@gmail.com>
2023-12-29 00:05:56 -0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:iosapp/HomeScreen.dart';
+import 'package:iosapp/Browse.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: HomeScreen(),
+      home: Browse(),
     );
   }
 }
