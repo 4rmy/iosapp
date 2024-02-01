2f10a0e4d3ac28011981e6241feeaef6887a28cc
4rmy <hjacob215@gmail.com>
2024-01-31 19:55:59 -0500

    Update Browse.dart

--- lib/Browse.dart
+++ lib/Browse.dart
@@ -1,5 +1,7 @@
 ///File download from FlutterViz- Drag and drop a tools. For more details visit https://flutterviz.io/
 
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 class Browse extends StatelessWidget {
@@ -84,219 +86,7 @@ class Browse extends StatelessWidget {
                               mainAxisAlignment: MainAxisAlignment.start,
                               crossAxisAlignment: CrossAxisAlignment.center,
                               mainAxisSize: MainAxisSize.max,
-                              children: [
-/*
-
-
-
-
-
-
-
-
-Children Here
-
-
-
-
-
-
-
-
-
-
-*/
-                                Container(
-                                  alignment: Alignment.center,
-                                  margin: const EdgeInsets.fromLTRB(0, 5, 0, 0),
-                                  padding: const EdgeInsets.all(0),
-                                  width: MediaQuery.of(context).size.width,
-                                  height:
-                                      MediaQuery.of(context).size.height * 0.1,
-                                  decoration: BoxDecoration(
-                                    color: const Color(0x3e00fff5),
-                                    shape: BoxShape.rectangle,
-                                    borderRadius: BorderRadius.circular(15.0),
-                                    border: Border.all(
-                                        color: const Color(0x4d9e9e9e), width: 1),
-                                  ),
-                                  child: Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceEvenly,
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.center,
-                                    mainAxisSize: MainAxisSize.max,
-                                    children: [
-                                      ClipRRect(
-                                        borderRadius:
-                                            BorderRadius.circular(15.0),
-                                        child:
-
-                                            ///***If you have exported images you must have to copy those images in assets/images directory.
-                                            Image(
-                                          image: const NetworkImage(
-                                              "https://picsum.photos/250?image=9"),
-                                          height: MediaQuery.of(context)
-                                                  .size
-                                                  .height *
-                                              0.08,
-                                          fit: BoxFit.cover,
-                                        ),
-                                      ),
-                                      const Column(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        mainAxisSize: MainAxisSize.max,
-                                        children: [
-                                          Text(
-                                            "App Name",
-                                            textAlign: TextAlign.start,
-                                            overflow: TextOverflow.clip,
-                                            style: TextStyle(
-                                              fontWeight: FontWeight.w700,
-                                              fontStyle: FontStyle.normal,
-                                              fontSize: 14,
-                                              color: Color(0xff000000),
-                                            ),
-                                          ),
-                                          Text(
-                                            "App Subtitle",
-                                            textAlign: TextAlign.start,
-                                            overflow: TextOverflow.clip,
-                                            style: TextStyle(
-                                              fontWeight: FontWeight.w400,
-                                              fontStyle: FontStyle.normal,
-                                              fontSize: 12,
-                                              color: Color(0x5900e8ff),
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                      MaterialButton(
-                                        onPressed: () {},
-                                        color: const Color(0xff0c6b00),
-                                        elevation: 0,
-                                        shape: RoundedRectangleBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(100.0),
-                                          side: const BorderSide(
-                                              color: Color(0xff808080),
-                                              width: 1),
-                                        ),
-                                        padding: const EdgeInsets.all(16),
-                                        child: Text(
-                                          "Get",
-                                          style: const TextStyle(
-                                            fontSize: 14,
-                                            fontWeight: FontWeight.w700,
-                                            fontStyle: FontStyle.normal,
-                                          ),
-                                        ),
-                                        textColor: const Color(0xffffffff),
-                                        height: 5,
-                                        minWidth: 65,
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                                Container(
-                                  alignment: Alignment.center,
-                                  margin: const EdgeInsets.fromLTRB(0, 5, 0, 0),
-                                  padding: const EdgeInsets.all(0),
-                                  width: MediaQuery.of(context).size.width,
-                                  height:
-                                      MediaQuery.of(context).size.height * 0.1,
-                                  decoration: BoxDecoration(
-                                    color: const Color(0x3e00fff5),
-                                    shape: BoxShape.rectangle,
-                                    borderRadius: BorderRadius.circular(15.0),
-                                    border: Border.all(
-                                        color: const Color(0x4d9e9e9e), width: 1),
-                                  ),
-                                  child: Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceEvenly,
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.center,
-                                    mainAxisSize: MainAxisSize.max,
-                                    children: [
-                                      ClipRRect(
-                                        borderRadius:
-                                            BorderRadius.circular(15.0),
-                                        child:
-
-                                            ///***If you have exported images you must have to copy those images in assets/images directory.
-                                            Image(
-                                          image: const NetworkImage(
-                                              "https://picsum.photos/250?image=9"),
-                                          height: MediaQuery.of(context)
-                                                  .size
-                                                  .height *
-                                              0.08,
-                                          fit: BoxFit.cover,
-                                        ),
-                                      ),
-                                      const Column(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        mainAxisSize: MainAxisSize.max,
-                                        children: [
-                                          Text(
-                                            "App Name",
-                                            textAlign: TextAlign.start,
-                                            overflow: TextOverflow.clip,
-                                            style: TextStyle(
-                                              fontWeight: FontWeight.w700,
-                                              fontStyle: FontStyle.normal,
-                                              fontSize: 14,
-                                              color: Color(0xff000000),
-                                            ),
-                                          ),
-                                          Text(
-                                            "App Subtitle",
-                                            textAlign: TextAlign.start,
-                                            overflow: TextOverflow.clip,
-                                            style: TextStyle(
-                                              fontWeight: FontWeight.w400,
-                                              fontStyle: FontStyle.normal,
-                                              fontSize: 12,
-                                              color: Color(0x5900e8ff),
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                      MaterialButton(
-                                        onPressed: () {},
-                                        color: const Color(0xff0c6b00),
-                                        elevation: 0,
-                                        shape: RoundedRectangleBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(100.0),
-                                          side: const BorderSide(
-                                              color: Color(0xff808080),
-                                              width: 1),
-                                        ),
-                                        padding: const EdgeInsets.all(16),
-                                        child: Text(
-                                          "Get",
-                                          style: const TextStyle(
-                                            fontSize: 14,
-                                            fontWeight: FontWeight.w700,
-                                            fontStyle: FontStyle.normal,
-                                          ),
-                                        ),
-                                        textColor: const Color(0xffffffff),
-                                        height: 5,
-                                        minWidth: 65,
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ],
+                              children: getApps(),
                             ),
                           ),
                         ),
@@ -385,5 +175,108 @@ Children Here
 
 
 List<Container> getApps() {
+  final res = HttpClient().getUrl(Uri.parse('https://api.github.com/repos/4rmy/iosapp/git/trees/main?recursive=1'));
+
   return [];
 }
+
+
+/*
+
+Container(
+  alignment: Alignment.center,
+  margin: const EdgeInsets.fromLTRB(0, 5, 0, 0),
+  padding: const EdgeInsets.all(0),
+  width: MediaQuery.of(context).size.width,
+  height:
+      MediaQuery.of(context).size.height * 0.1,
+  decoration: BoxDecoration(
+    color: const Color(0x3e00fff5),
+    shape: BoxShape.rectangle,
+    borderRadius: BorderRadius.circular(15.0),
+    border: Border.all(
+        color: const Color(0x4d9e9e9e), width: 1),
+  ),
+  child: Row(
+    mainAxisAlignment:
+        MainAxisAlignment.spaceEvenly,
+    crossAxisAlignment:
+        CrossAxisAlignment.center,
+    mainAxisSize: MainAxisSize.max,
+    children: [
+      ClipRRect(
+        borderRadius:
+            BorderRadius.circular(15.0),
+        child:
+
+            ///***If you have exported images you must have to copy those images in assets/images directory.
+            Image(
+          image: const NetworkImage(
+              "https://picsum.photos/250?image=9"),
+          height: MediaQuery.of(context)
+                  .size
+                  .height *
+              0.08,
+          fit: BoxFit.cover,
+        ),
+      ),
+      const Column(
+        mainAxisAlignment:
+            MainAxisAlignment.center,
+        crossAxisAlignment:
+            CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Text(
+            "App Name",
+            textAlign: TextAlign.start,
+            overflow: TextOverflow.clip,
+            style: TextStyle(
+              fontWeight: FontWeight.w700,
+              fontStyle: FontStyle.normal,
+              fontSize: 14,
+              color: Color(0xff000000),
+            ),
+          ),
+          Text(
+            "App Subtitle",
+            textAlign: TextAlign.start,
+            overflow: TextOverflow.clip,
+            style: TextStyle(
+              fontWeight: FontWeight.w400,
+              fontStyle: FontStyle.normal,
+              fontSize: 12,
+              color: Color(0x5900e8ff),
+            ),
+          ),
+        ],
+      ),
+      MaterialButton(
+        onPressed: () {},
+        color: const Color(0xff0c6b00),
+        elevation: 0,
+        shape: RoundedRectangleBorder(
+          borderRadius:
+              BorderRadius.circular(100.0),
+          side: const BorderSide(
+              color: Color(0xff808080),
+              width: 1),
+        ),
+        padding: const EdgeInsets.all(16),
+        child: Text(
+          "Get",
+          style: const TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w700,
+            fontStyle: FontStyle.normal,
+          ),
+        ),
+        textColor: const Color(0xffffffff),
+        height: 5,
+        minWidth: 65,
+      ),
+    ],
+  ),
+),
+
+*/*/
